25f329d2f9666165a1170d4b8a7b84a385cdff24
stho32 <stho32github@gmail.com>
2022-01-03 21:23:14 +0100

    draft for looking up inheritance of classes

--- Source/code2json/code2json.Console/Program.cs
+++ Source/code2json/code2json.Console/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+// try this next time:
+// https://stackoverflow.com/questions/28234052/finding-all-class-declarations-than-inherit-from-another-with-roslyn
+
+
+var directory = Directory.GetCurrentDirectory();
+directory = directory.Remove(directory.IndexOf("Source"));
+
+var files = Directory.GetFiles(directory, "*.cs", SearchOption.AllDirectories);
+
+Console.WriteLine($"Starting analysis... {directory}...");
+
+var classes = new ClassDeclarationCollector();
+
+foreach (var file in files)
+{
+    var code = File.ReadAllText(file);
+    var tree = CSharpSyntaxTree.ParseText(code);
+    
+    classes.Visit(tree.GetCompilationUnitRoot());
+}
+
+foreach (var someClass in classes.classDeclarations)
+{
+    Console.WriteLine($"- class {someClass.Identifier.Text}");
+    if (someClass?.BaseList?.Types != null)
+    {
+        foreach (var derivedFromType in someClass.BaseList.Types)
+        {
+            Console.WriteLine($"    <- {derivedFromType.GetText().ToString().Trim()}");
+        }
+    }
+}
+
+Console.WriteLine("Finished.");
+
+
+class ClassDeclarationCollector : CSharpSyntaxWalker
+{
+    public ICollection<ClassDeclarationSyntax> classDeclarations { get; } = new List<ClassDeclarationSyntax>();
+
+    public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+    {
+        classDeclarations.Add(node);
+    }
+}
+
+public interface ISomeThing
+{
+    void Hey();
+}
+
+public abstract class AnotherClass : ISomeThing
+{
+    public abstract void Hey();
+}
+
+public class MoreOfAnotherClass : AnotherClass
+{
+    public override void Hey()
+    {
+        Console.WriteLine("Dudelidu");
+    }
+}
